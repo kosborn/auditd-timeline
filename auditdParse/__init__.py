b865bc723f1d28f07c08d339a244157742c005da
Kos <kos@kos.io>
2014-01-29 00:47:16 -0800

    Cleaned up some redundant code. Now fully generates sql queries based off of yaml config file

--- auditdParse/__init__.py
+++ auditdParse/__init__.py
@@ -4,37 +4,43 @@ import sqlite3, re, sys, os, yaml
 
 class auditdParse:
 	def __init__(self,auditFile):
-		typeConf = open('types.yaml','r').read()
-		typeList = yaml.load(typeConf)
+		# For debugging or general verbosity
+		# 0 = almost no output
+		# 1 = simple error (such as undefined message type)
+		# 2 = detailed messages with those errors
+		self.verboseLevel=0
 
 		# Here is where we defined expected database & dictionary parameters
 		# We define these and merge the actual messages just
 		# in case they do not contain one of the parameters
 		# It must be (expected[n] + message) or it will overwrite
 		# Defined in yaml config file
+		typeConf = open('types.yaml','r').read()
+		typeList = yaml.load(typeConf)
+		self.expected, self.createDB, self.insertDB = {},{},{}
 
-		self.expected = {}
-		self.createDB = {}
 		for i in typeList['auditd'].keys():
 			self.expected[i] = {k: None for k in typeList['auditd'][i]['parameters']}
 			self.createDB[i] = 'CREATE TABLE '+str(i)+'('+', '.join(list(' '.join([key,value]) for key,value in typeList['auditd'][i]['parameters'].items()))+');'
+			self.insertDB[i] = 'INSERT INTO '+str(i)+' VALUES(:'+', :'.join(typeList['auditd'][i]['parameters'].keys())+');'
 
-		self.log = open(auditFile,'r').readlines()
+		# Check to see if the sqlite db exists, create it otherwise
 		if os.path.isfile('audit.db'):
-			self.con = sqlite2.connect('audit.db')
+			self.con = sqlite3.connect('audit.db')
 			self.cur = self.con.cursor()
 		else:
 			self.con = sqlite3.connect('audit.db')
 			self.cur = self.con.cursor()
-			#sqlSchema = open(os.path.dirname(__file__)+'/audit.sql','r').read()
+			# thanks to code above, tables are dynamically generated from the yaml config
 			self.cur.executescript(' '.join(self.createDB.values()))
 			self.con.commit()
 
 
-		for i in self.log:
-			message = self.parseLine(i)
+		for line in open(auditFile,'r').readlines():
+			message = self.parseLine(line)
 			if message:
 				self.insertType(message)
+
 		self.con.commit()
 		self.con.close()
 
@@ -43,10 +49,14 @@ class auditdParse:
 	 	if line.strip():
 			try:
 				message={x: y for(x, y) in re.findall('([a-z0-9]+)=([^ ]+)+',line)}
-				message['aid'] = re.match('audit\([^:]+:([^)]+)\)',message['msg']).groups()[0]
-				message['timestamp'] = re.match('audit\(([^.]+).+\)',message['msg']).groups()[0]
+				if message['type'] in self.expected.keys():
+					message['aid'] = re.match('audit\([^:]+:([^)]+)\)',message['msg']).groups()[0]
+					message['timestamp'] = re.match('audit\(([^.]+).+\)',message['msg']).groups()[0]
+				else:
+					return False
 			except Exception as error:
-				print "Some error occured: "+error.message
+				self.loud("Error!",message,0)
+				self.loud("Some error occured",error.message,0)
 
 			return message
 		else:
@@ -54,36 +64,40 @@ class auditdParse:
 
 	def insertType(self,message):
 		atype = message['type']
-		if atype == "SYSCALL":
-			message = dict(self.expected[atype].items()+message.items())
-			query = "INSERT INTO SYSCALL VALUES(:aid,:timestamp,:syscall,:success,:exit,:items,:ppid,:pid,:auid,:uid,:gid,:euid,:suid,:fsuid,:egid,:sgid,:fsgid,:tty,:ses,:comm,:exe)";
-		elif atype == "EXECVE":
-			message = dict(self.expected[atype].items()+message.items())
-			argc = int(message['argc'])
+		message = dict(self.expected[atype].items()+message.items())
+
+		# Since EXECVE contains items we want (parameters) in a0, a1, a2, etc
+		# format, we must group them into a single string to avoid creating
+		# many columns in the SQL database
+		if atype == "EXECVE":
+			argc = int(message['argc']) if message['argc'] is not None else 0
 			tmp={}
 			counter=0
+			# Merge all a[0-9] together. We could probably regex this isntead
 			try:
 				while counter < argc:
-					tmp[counter] = message['a'+str(counter)]
+					# Sometimes an argument is missing or on the next line
+					# We need to handle this if it's on the next line somehow
+					try:
+						tmp[counter] = message['a'+str(counter)]
+					except:
+						tmp[counter]=None
 					counter = counter+1
 				message['argdata'] = str(tmp)
 			except Exception as error:
-				print message
-				print "error: "+str(error)
-			query = "INSERT INTO EXECVE VALUES(:aid,:timestamp,:argc,:argdata);"
-		elif atype == "CWD":
-			message = dict(self.expected[atype].items()+message.items())
-			query = "INSERT INTO CWD VALUES(:aid,:timestamp,:cwd);"
-		elif atype == "PATH":
-			message = dict(self.expected[atype].items()+message.items())
-			query = "INSERT INTO PATH VALUES(:aid,:timestamp,:item,:name,:inode,:dev,:mode,:ouid,:ogid);"
+				self.loud("Error!",message,0)
+				self.loud("Some error occured",error.message,0)
 		else:
-			#print "Unknown type: "+str(atype)
-			query = "";
-			#sys.exit()
+			# Since this is likely to happen a lot, don't print this message by default
+			self.loud("Unknown type",atype)
+			self.loud("Message",message,2)
 
-		self.cur.execute(query,message) 
+		self.cur.execute(self.insertDB[atype],message) 
 
+
+	def loud(self,note,item,level=1):
+		if level <= self.verboseLevel:
+			print note+": "+str(item)
 
 
 doit = auditdParse('/tmp/audit.log')
--- auditdParse/audit.sql
+++
@@ -1,7 +0,0 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE SYSCALL(aid int, timestamp int, syscall int, success BOOLEAN, exit int, items int, ppid int, pid int, auid int, uid int, gid int, euid int, suid int, fsuid int, egid int, sgid int, fsgid int, tty varchar(10), ses int, comm text, exe text);
-CREATE TABLE EXECVE(aid int, timestamp int, argc int, argdata text);
-CREATE TABLE PATH(aid int, timestamp int, item int, name text, inode int, dev text, mode text, ouid int, ogid int);
-CREATE TABLE CWD(aid int, timestamp int, cwd text);
-COMMIT;
